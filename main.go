2689308ce651055a29f84b7ac256c09d86115d3e
Relevantfender <ilic.miroslav1993@gmail.com>
2025-07-13 00:15:19 +0200

    added new routes and handler funcs before adding middleware

--- main.go
+++ main.go
@@ -2,16 +2,42 @@ package main
 
 import (
 	// "fmt"
+
+	"log"
 	"net/http"
+	"sync/atomic"
 )
 
+type apiConfig struct {
+	fileServerHits atomic.Int32
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+
+}
+
 func main() {
+	const filepathRoot = "."
+	const port = "8080"
+
 	mux := http.NewServeMux()
 
-	server := http.Server{
+	mux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir(filepathRoot))))
+	mux.HandleFunc("/healthz", handlerReadiness)
+
+	server := &http.Server{
+		Addr:    ":" + port,
 		Handler: mux,
-		Addr:    ":8080",
 	}
 
-	server.ListenAndServe()
+	log.Printf("Serving files from %s on port: %s\n", filepathRoot, port)
+	log.Fatal(server.ListenAndServe())
+
+}
+
+func handlerReadiness(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(http.StatusText(http.StatusOK)))
+
 }
